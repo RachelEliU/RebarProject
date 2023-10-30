024424a60355ac7a9522458be8b890dccb2085d6
Rachel Ukelosn <rachel4171165@gmail.com>
2023-10-30 18:53:03 +0200

    Creating new project and building basic classes

---
+++ RebarProject/RebarProject/Models/Account.cs
@@ -0,0 +1,9 @@
+﻿namespace RebarProject.Models
+{
+    public class Account
+    {
+        public Guid Id { get; set; }
+        public List<Order> Orders { get; set; }= new List<Order>();
+        public double sum {  get; set; }
+    }
+}
---
+++ RebarProject/RebarProject/Models/Shake.cs
@@ -0,0 +1,30 @@
+﻿
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace RebarProject.Models
+{
+    [BsonIgnoreExtraElements]
+    public class Shake
+   {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public Guid Id { get; }
+        [BsonElement("name")]
+        public string Name { get; set; }
+        [BsonElement("description")]
+
+        public string Description { get; set; }
+        [BsonElement("priceL")]
+        public int PriceL { get; set; }
+        [BsonElement("priceM")]
+
+        public int PriceM { get; set; }
+        [BsonElement("priceS")]
+
+        public int PriceS { get; set; }
+        [BsonElement("ingredients")]
+
+        public List<Ingredient> Ingredients { get; set; }       
+    }
+}
